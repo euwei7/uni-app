f9878d334a2797237176b7666176199115aee87d
Euwei <120472761+euwei7@users.noreply.github.com>
2023-11-12 17:29:47 +0800

    Add files via upload

---
+++ src/components/hello/hello.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+
+
+</script>
+<template>
+ <view>hello</view>
+</template>
+<style>
+  
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+
+
+
+</script>
+<template>
+ <view>
+  <hello></hello>
+ </view>
+</template>
+<style>
+  
+</style>
